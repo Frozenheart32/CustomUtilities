3781e10a660c3ecc5230df57b48877d961addcc9
Frozenheart32 <alexroid32@gmail.com>
2023-10-04 23:16:55 +0300

    Install project

---
+++ Source/CustomUtilities/CustomUtilitiesGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CustomUtilitiesGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACustomUtilitiesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACustomUtilitiesGameMode();
+};
+
+
+
